90fe19e920c1b7f037fca757ad7e684148a0b248
Brian Girko <brian.girko@gmail.com>
2020-04-19 15:52:48 +0200

    version 0.1.2

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/* global File */
 'use strict';
 
 const notify = e => chrome.notifications.create({
@@ -69,50 +70,89 @@ chrome.runtime.onMessage.addListener(async request => {
         type.push('audio');
       }
       chrome.desktopCapture.chooseDesktopMedia(type, async streamId => {
-        const constraints = {
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: streamId
-            }
-          }
-        };
-        if (request.audio === 'system') {
-          constraints.audio = {
-            mandatory: {
-              chromeMediaSource: 'system',
-              chromeMediaSourceId: streamId
+        try {
+          const constraints = {
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: streamId
+              }
             }
           };
-        }
-
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        if (request.audio === 'mic') {
-          const audio = await navigator.mediaDevices.getUserMedia({
-            audio: true
-          });
-          for (const track of audio.getAudioTracks()) {
-            stream.addTrack(track);
+          if (request.audio === 'system') {
+            constraints.audio = {
+              mandatory: {
+                chromeMediaSource: 'system',
+                chromeMediaSourceId: streamId
+              }
+            };
           }
-        }
-        tracks = stream.getTracks();
 
-        const mediaRecorder = new MediaRecorder(stream, {
-          mime: 'video/webm'
-        });
-        mediaRecorder.addEventListener('dataavailable', e => {
-          const url = URL.createObjectURL(e.data);
-          chrome.downloads.download({
-            filename: 'capture.webm',
-            url
-          }, () => URL.revokeObjectURL(url));
-          window.clearTimeout(button.id);
-        });
-        stream.oninactive = stream.onremovetrack = stream.onended = () => {
-          mediaRecorder.stop();
-        };
-        mediaRecorder.start();
-        button.recording();
+          const stream = await navigator.mediaDevices.getUserMedia(constraints);
+          if (request.audio === 'mic') {
+            const audio = await navigator.mediaDevices.getUserMedia({
+              audio: true
+            });
+            for (const track of audio.getAudioTracks()) {
+              stream.addTrack(track);
+            }
+          }
+          tracks = stream.getTracks();
+
+          const file = new File();
+          await file.open();
+          const mediaRecorder = new MediaRecorder(stream, {
+            mime: 'video/webm'
+          });
+          const capture = () => {
+            mediaRecorder.requestData();
+            capture.id = setTimeout(capture, 5000);
+          };
+          capture.offset = 0;
+          capture.progress = 0;
+
+          mediaRecorder.addEventListener('error', e => notify(e.message));
+          mediaRecorder.addEventListener('dataavailable', e => {
+            const download = () => {
+              if (capture.progress === 0 && mediaRecorder.state === 'inactive') {
+                window.clearTimeout(button.id);
+
+                file.download('capture.webm', 'video/webm').then(() => file.remove()).catch(e => {
+                  console.warn(e);
+                  notify('An error occurred during saving: ' + e.message);
+                });
+              }
+            };
+            if (e.data.size) {
+              capture.progress += 1;
+              const reader = new FileReader();
+              reader.onload = e => {
+                file.chunks({
+                  offset: capture.offset,
+                  buffer: new Uint8Array(e.target.result)
+                }).then(() => {
+                  capture.progress -= 1;
+                  download();
+                });
+              };
+              reader.readAsArrayBuffer(e.data);
+              capture.offset += 1;
+            }
+            else {
+              download();
+            }
+          });
+          stream.oninactive = stream.onremovetrack = stream.onended = () => {
+            clearTimeout(capture.id);
+            mediaRecorder.stop();
+          };
+          mediaRecorder.start();
+          button.recording();
+          capture();
+        }
+        catch (e) {
+          notify(e.message || 'Capturing Failed with an unknown error');
+        }
       });
     }
     catch (e) {
@@ -123,3 +163,56 @@ chrome.runtime.onMessage.addListener(async request => {
     notify(request.message);
   }
 });
+
+// save files from indexdb and remove the
+{
+  const restore = async () => {
+    const os = 'databases' in indexedDB ? await indexedDB.databases() : Object.keys(localStorage)
+      .filter(name => name.startsWith('file:'))
+      .map(name => ({
+        name: name.replace('file:', '')
+      }));
+    for (const o of os) {
+      const file = new File(o.name);
+      await file.open();
+      try {
+        await file.download('capture.webm', 'video/webm');
+      }
+      catch (e) {
+        console.warn(e);
+      }
+      file.remove();
+    }
+  };
+
+  chrome.runtime.onStartup.addListener(restore);
+  chrome.runtime.onInstalled.addListener(restore);
+}
+
+
+/* FAQs & Feedback */
+{
+  const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
+  if (navigator.webdriver !== true) {
+    const page = getManifest().homepage_url;
+    const {name, version} = getManifest();
+    onInstalled.addListener(({reason, previousVersion}) => {
+      management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
+        'faqs': true,
+        'last-update': 0
+      }, prefs => {
+        if (reason === 'install' || (prefs.faqs && reason === 'update')) {
+          const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
+          if (doUpdate && previousVersion !== version) {
+            tabs.create({
+              url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
+              active: reason === 'install'
+            });
+            storage.local.set({'last-update': Date.now()});
+          }
+        }
+      }));
+    });
+    setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+  }
+}
